fac7d48d7e3a656a31242889f54b53414049975e
aditigandhi <45218230+aditigandhi@users.noreply.github.com>
2021-09-23 16:46:57 -0700

    Flixster Part 2 Files

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Aditi Gandhi on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
